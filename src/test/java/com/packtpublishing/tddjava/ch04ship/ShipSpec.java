e7ca160e30ac2935d8f96afe5778f30ce607633f
Bruno Buck <b.brigagao@gmail.com>
2023-10-05 20:05:39 -0300

    Testing ship

--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -7,4 +7,18 @@ import static org.testng.Assert.*;
 @Test
 public class ShipSpec {
 
+    Ship ship;
+    Point point;
+    Location location;
+
+    @BeforeMethod
+    public void setup() {
+        this.point = new Point(0, 0);
+        this.location = new Location(this.point, Direction.NORTH);
+        this.ship = new Ship(this.location);
+    }
+
+    public void given_new_ship_then_location_is_set() {
+        assertEquals(this.ship.location(), this.location);
+;    }
 }
